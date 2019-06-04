c60bdb4adc0bffb7b947fa923963b6799a973a07
Dona Jose <donajose@expressbase.com>
2019-06-04 15:30:43 +0530

    Assembly Info

--- Startup.cs
+++ Startup.cs
@@ -19,6 +19,7 @@ using ServiceStack.RabbitMq;
 using ServiceStack.Redis;
 using System;
 using System.IdentityModel.Tokens.Jwt;
+using System.Reflection;
 
 namespace ExpressBase.StaticFileServer
 {
@@ -105,6 +106,12 @@ namespace ExpressBase.StaticFileServer
                Environment.GetEnvironmentVariable(EnvironmentConstants.EB_REDIS_PORT));
 
             container.Register<IRedisClientsManager>(c => new RedisManagerPool(redisConnectionString));
+            //Setting Assembly version in Redis
+            RedisClient client = (container.Resolve<IRedisClientsManager>() as RedisManagerPool).GetClient() as RedisClient;
+            AssemblyName assembly = Assembly.GetExecutingAssembly().GetName();
+            String version = assembly.Name.ToString() + " - " + assembly.Version.ToString();
+            client.Set("FileServerAssembly", version);
+
             container.Register<IEbConnectionFactory>(c => new EbConnectionFactory(c)).ReusedWithin(ReuseScope.Request);
             container.Register<IEbServerEventClient>(c => new EbServerEventClient()).ReusedWithin(ReuseScope.Request);
             container.Register<IEbMqClient>(c => new EbMqClient()).ReusedWithin(ReuseScope.Request);
